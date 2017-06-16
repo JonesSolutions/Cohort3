cdf1ad2adda0d9f9a0cd34dfaba88cc694a47d27
Brian <Brian@JOnessolutions.com>
2017-06-15 21:20:58 -0600

    Complex data model

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using ContosoUniversity.Validation;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
+        public Student()
+        {
+
+        }
         public int ID { get; set; }
 
         [Required]
@@ -27,6 +32,8 @@ namespace ContosoUniversity.Models
         [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:yyyy-MM-dd}")]
         [DataType(DataType.Date, ErrorMessage ="Please enter a valid date (yyyy/MM/DD)")]
         [Display(Name ="Enrollment Date")]
+        //[Range(typeof(DateTime),"01/01/1900", "01/01/2016")]
+        [ValidateDateRange(FirstDate = "01/10/2008", SecondDate = "01/01/2010", ErrorMessage ="Please enter a date in the valid range.")]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
